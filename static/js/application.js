be8e46b561c9189f5423e09e6fdc337708a30277
Vanshita99 <46200046+Vanshita99@users.noreply.github.com>
2019-02-27 02:35:03 -0800

    Update application.js

--- static/js/application.js
+++ static/js/application.js
@@ -2,14 +2,18 @@
 $(document).ready(function(){
     //connect to the socket server.
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
-    var numbers_received = [];
+    var str1 = "data:image/png;base64,";
 
     //receive details from server
     socket.on('newnumber', function(msg) {
-        console.log("Received number" + msg.number);
-        //maintain a list of ten numbers
-        $("#r_part").html("<img src='data:image/gif;base64," + base64encode(msg.number) +"' />");
-       
+        console.log("received");
+        
+        $.ajax({url: "get_image", success: function(result){
+            $('#div_img').html("");
+            $('#div_img').html('<img src="data:image/png;base64,' + result + '" />');
+        }});
+        
     });
 
 });
+
