02cc8155a1c8869f523575bd56a71616ca5da524
Vanshita99 <Vanshita99@github.com>
2019-06-08 21:52:16 +0530

    Added code for selecting model using radion button

--- static/js/application.js
+++ static/js/application.js
@@ -26,6 +26,32 @@ $(document).ready(function(){
         });
   }); 
 
+  $("#settings input[name='model_type']").click(function(){
+    alert('You clicked radio!');
+    if($('input:radio[name=model_type]:checked').val() == "cnn"){
+        alert($('input:radio[name=type]:checked').val());
+        //$('#select-table > .roomNumber').attr('enabled',false);
+    }
+});
+
+  $('#cnn').on('click', function() {
+    var settingObject = new Object();
+    settingObject.cnn = true;
+    settingObject.lstm = false;
+    send_settings(JSON.stringify(settingObject));
+  });
+
+  $('#lstm').on('click', function() {
+    var settingObject = new Object();
+    settingObject.cnn = false;
+    settingObject.lstm = true;
+    send_settings(JSON.stringify(settingObject));
+  });
+
+  
+
+
+
   $('#stop').on('click',function(){
         socket.disconnect();
         console.log("stopping");
