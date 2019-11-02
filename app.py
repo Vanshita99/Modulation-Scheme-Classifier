1e630bf7ffd371b9e98ebb57a774ad904b861d9e
Vanshita99 <Vanshita99@github.com>
2019-11-02 16:43:39 +0530

    calculating accuracy

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ class RandomThread(Thread):
         #eng = matlab.engine.start_matlab()
         total_matched_instances=0
         total_instances=0
-        
+        accuracy=0
         
         band_idx=np.empty((no_of_bands,1))
         bar_height=np.empty((no_of_bands,1))
