adbf5f02f7eb4e5cf4197cd9013943509e5232e7
Vanshita99 <Vanshita99@github.com>
2019-11-02 13:15:59 +0530

    Revert "calculating accuracy"
    This reverts commit 31a144529a108ba1e57d70cec21b15f63594f669.

--- app.py
+++ app.py
@@ -245,7 +245,6 @@ class RandomThread(Thread):
                         ax.plot(x,y)
                         fig.savefig("test.png")
                         socketio.emit('newnumber', {'number': "test.png"}, namespace='/test')
-                        plt.pause(1)
                         ax.clear()
                         plt.pause(1)
                     else:
@@ -265,9 +264,7 @@ class RandomThread(Thread):
                         ax.text(BW1*(BN1-1), height+10,accuracy)
                         fig.savefig("test.png")
                         socketio.emit('newnumber', {'number': "test.png"}, namespace='/test')
-                        plt.pause(1)
                         ax.clear()
-                        plt.pause(1)
                     
             except:
                 continue        
