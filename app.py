163f59a55abb2e1e6407c725bc8eebdfbc419c4a
Vanshita99 <Vanshita99@github.com>
2019-11-07 03:36:33 +0530

    calculating accuracy

--- app.py
+++ app.py
@@ -206,6 +206,8 @@ class RandomThread(Thread):
                 #calculateMatchedInstances(modulation_schemes)
                 if checkSNRChange==True:
                      accuracy=0
+                     global checkSNRChange 
+                     checkSNRChange= False;
                 if i%2==0:
 
                     a,v,w,x,BW,z_IQ,z_AP =eng.testFunction(no_of_bands,channel,SNR,nargout=7)
